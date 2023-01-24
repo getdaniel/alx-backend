62a4fc9d44c799748dfc9fe3f42a0937bfd07774
Me <getdaniel>
2023-01-24 22:18:32 +0300

    remove extraspace

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -20,7 +20,7 @@ class LIFOCache(BaseCaching):
         if key not in self.cache_data:
             if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
                 last_key, _ = self.cache_data.popitem(True)
-                print("DISCARD: ", last_key)
+                print("DISCARD:", last_key)
         self.cache_data[key] = item
         self.cache_data.move_to_end(key, last=True)
 
