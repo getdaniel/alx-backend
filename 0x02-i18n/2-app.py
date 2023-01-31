b432501be3469c7e06613d49ddce38c6ffde42c4
Me <getdaniel>
2023-01-31 12:18:41 +0300

    modify and update

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -22,6 +22,7 @@ def get_index() -> str:
     """ Renders the index.html file."""
     return render_template('2-index.html')
 
+@babel.localeselector
 def get_locale() -> str:
     """ Get locale from request."""
     return request.accept_languages.best_match(app.config["LANGUAGES"])
--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -20,8 +20,9 @@ babel = Babel(app)
 @app.route('/')
 def get_index() -> str:
     """ Renders the index.html file."""
-    return render_template('2-index.html')
+    return render_template('3-index.html')
 
+@babel.localeselector
 def get_locale() -> str:
     """ Get locale from request."""
     return request.accept_languages.best_match(app.config["LANGUAGES"])
